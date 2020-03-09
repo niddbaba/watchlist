05175866b13050774f491642d7756fcaca9cbb13
max wu <maxwupro@outlook.com>
2020-03-10 01:09:03 +0800

    new addition

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
-from flask import Flask, escape, url_for
+from flask import Flask, render_template
 '''
-由模块名字默认程序名字
+由模块名字默认程序名
 '''
 app = Flask(__name__)
 
+# 准备数据呈现在网页上
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 
 @app.route('/')
-def hello():
-    return '<h1>Welcome to my personal website!</h1>'
-
-
-@app.route('/user/<name>')
-def user_page(name):
-    # 解析html文件显示在网站上
-    return "user :%s" % escape(name)
-
-
-@app.route('/test')
-def test_url_for():
-    # url_for参数为函数名字，用于打印执行该函数的地址
-
-    print(url_for('hello'))
-    print(url_for('user_page', name='weige'))
-    print(url_for('test_url_for'))
-    print(url_for('test_url_for', num=2))
-    return 'Test Page'
+def index():
+    return render_template('index.html', name=name, movies=movies)
