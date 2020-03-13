f9196549df8c1b41dc35808ce407951afb495d31
max wu <maxwupro@outlook.com>
2020-03-14 00:53:40 +0800

    他妈的CSS有毒吧？

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, escape
+from flask import Flask, render_template, escape, redirect, url_for, flash, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 import click
@@ -21,7 +21,7 @@ def initdb(drop):
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + \
     os.path.join(app.root_path, 'data.db')
-
+app.config['SECRET_KEY'] = 'dev'
 # ////数据库文件的绝对地址
 db = SQLAlchemy(app)  # 初始化扩展，传入程序实例
 
@@ -78,7 +78,56 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        title = request.form.get('title')  # 请求的结果是一个字典，所以用get返回值
+        year = request.form.get('year')
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item Created.')
+        return redirect(url_for('index'))
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
+
+
+@app.route('movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':  # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+
+        movie.title = title  # 更新标题
+        movie.year = year  # 更新年份
+        db.session.commit()  # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
+
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))  # 重定向回主页
+
+
+@app.route('/delete_first')
+def delete_first():
+    movie = Movie.query.first()
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for('index'))
